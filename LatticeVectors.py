2c7a3d7e3d64ac652853c8563e311c4ce7d6ffd4
bmondal94 <56228126+bmondal94@users.noreply.github.com>
2021-08-14 12:32:35 +0200

    update-1

--- LatticeVectors.py
+++ LatticeVectors.py
@@ -2,33 +2,17 @@
 import numpy as np
 import argparse
 import sys
-
+# -------------------------------- Parsers ------------------------------------------
 parser = argparse.ArgumentParser(prog='LatticeVectors', description='This script calculates the different cell parameters from VASP file (POSCAR/CONTCAR format)',
 					epilog='Have fun!')
 parser.add_argument('filename', metavar='FILENAME', type=argparse.FileType('r'), help='The file path e.g. /home/mondal/VASP/test.vasp')
+parser.add_argument('-UC', action='store_true', default=False, help='Is the cell already a conventional unit cell? True/False (default: False)')
 parser.add_argument('-SC', action='store_true', default=False, help='Is the cell a supercell? True/False (default: False)')
 parser.add_argument('-SF', nargs='+', type=int, default=[6,6,6], help='Supercell dimensions (must be int) in a, b and c lattice vector directions respectively. (default: [6,6,6])')
 parser.add_argument('-CS', type=str, default='cubic', help='Crystal Structure e.g. cubic, zincblende, strechedzincblende etc. (default: cubic)')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 
-if len(sys.argv)==1:
-	parser.print_help()
-	sys.exit(0)
-#try:
-#	args = parser.parse_args()
-#except:
-#	#parser.print_help()
-#	sys.exit(0)
-
-args = parser.parse_args()
-
-filename = args.filename
-SuperCell = args.SC
-S_factor = np.asarray(args.SF, dtype=np.float)
-cst = args.CS
-
-print('\nFILENAME\t=\t%s\nSupercell\t=\t%s'%(filename.name,SuperCell),'\nSupercell Dimensions\t=\t',S_factor,'\n')
-
+# ---------------------------------Functions -------------------------------------------
 def vec(a):
 	return (np.sqrt(np.sum(a**2)))
 
@@ -70,46 +54,74 @@ def lattice_parameter(vector,crys_str='cubic'):
 	print('Crystal Structure (conventional unit cell)\t=\t%s\n'%crys_str)
 	return (lp)
 
-lines = filename.readlines()
-factor = float(lines[1].split()[0])
-a = np.asarray(lines[2].split()).astype(np.float)
-b = np.asarray(lines[3].split()).astype(np.float)
-c = np.asarray(lines[4].split()).astype(np.float)
-N_atom = np.sum(np.asarray(lines[6].split()).astype(np.int))
+# ----------------------------------------------------------------------------------------
 
-vector = np.array([vec(a),vec(b),vec(c)])
-vector *= factor
+if __name__=="__main__":
+	# ************************ Read parsers ***************************************
+	if len(sys.argv)==1:
+		parser.print_help()
+		sys.exit(0)
+	#try:
+	#	args = parser.parse_args()
+	#except:
+	#	#parser.print_help()
+	#	sys.exit(0)
 
-angle = np.array([anglefn(a,b), anglefn(b,c), anglefn(a,c)])
+	args = parser.parse_args()
 
-C_Volm = np.prod(vector)*np.sqrt(1. - np.sum(np.cos(angle)**2) + 2.*np.prod(np.cos(angle)))
+	filename = args.filename
+	UNIT_CELL = args.UC
+	SuperCell = args.SC
+	S_factor = np.asarray(args.SF, dtype=np.float)
+	cst = args.CS
 
-angle = np.degrees(angle)
+	# ********************** Read coordinate file ************************************
+	print('\nFILENAME\t=\t%s\nSupercell\t=\t%s'%(filename.name,SuperCell),'\nSupercell Dimensions\t=\t',S_factor,'\n')
 
-print('Cell parameters (Lattice vectors and angles):\n')
-print('Total number of atoms (/ions) in the cell\t=\t%d\n'%N_atom)
-print('a\t=\t%7.5f\nb\t=\t%7.5f\nc\t=\t%7.5f\n'%(vector[0],vector[1],vector[2]))
-print('alpha\t=\t%7.5f\nbeta\t=\t%7.5f\ngamma\t=\t%7.5f\n'%(angle[0],angle[1],angle[2]))
-print('Cell Volume\t=\t%8.4f\n'%C_Volm)
+	lines = filename.readlines()
+	factor = float(lines[1].split()[0])
+	a = np.asarray(lines[2].split()).astype(np.float)
+	b = np.asarray(lines[3].split()).astype(np.float)
+	c = np.asarray(lines[4].split()).astype(np.float)
+	N_atom = np.sum(np.asarray(lines[6].split()).astype(np.int))
+	
+	# ********************** Primitive --> Unit cell *********************************
+	vector = np.array([vec(a),vec(b),vec(c)])
+	vector *= factor
 
-if (SuperCell):
-	primitive_vector = np.divide(vector,S_factor)  #S_factor=Supercell factor
-else:
-	primitive_vector = vector
+	angle = np.array([anglefn(a,b), anglefn(b,c), anglefn(a,c)])
 
-print('Primitive cell lattice vectors:\n')
-print('a\t=\t%7.5f\nb\t=\t%7.5f\nc\t=\t%7.5f\n'%(primitive_vector[0],primitive_vector[1],primitive_vector[2]))
+	C_Volm = np.prod(vector)*np.sqrt(1. - np.sum(np.cos(angle)**2) + 2.*np.prod(np.cos(angle)))
 
-print("Note: The convention used here for lattice parameters calculation:\n\t 'a' should be in xy-plane \n\t 'b' should be in yz-plane \n\t 'c' hsould be in xz-plane.\n")
-print("** WARNING: If you don't follow above convention then depending on how you define lattice vectors the dummy index x, y, z in L may change\n")
-print("** You might need to rotate the coordinate system to follow the convention. Please check the lattice vectors in input file (e.g. POSCAR).\n")
+	angle = np.degrees(angle)
 
-rotate = input("If you need the rotation please provide here the number of rotation you need to get to the above condition. Else press 'ENTER'.\n") 
+	print('Cell parameters (Lattice vectors and angles):\n')
+	print('Total number of atoms (/ions) in the cell\t=\t%d\n'%N_atom)
+	print('a\t=\t%7.5f\nb\t=\t%7.5f\nc\t=\t%7.5f\n'%(vector[0],vector[1],vector[2]))
+	print('alpha\t=\t%7.5f\nbeta\t=\t%7.5f\ngamma\t=\t%7.5f\n'%(angle[0],angle[1],angle[2]))
+	print('Cell Volume\t=\t%8.4f\n'%C_Volm)
 
-if(rotate):
-	primitive_vector = np.roll(primitive_vector,int(rotate))
+	if (SuperCell):
+		primitive_vector = np.divide(vector,S_factor)  #S_factor=Supercell factor
+	else:
+		primitive_vector = vector
+	if  UNIT_CELL:
+		print("** Note: The lattice is already a conventional unit cell")
+		lattice = primitive_vector
+	else:
+		print('Primitive cell lattice vectors:\n')
+		print('a\t=\t%7.5f\nb\t=\t%7.5f\nc\t=\t%7.5f\n'%(primitive_vector[0],primitive_vector[1],primitive_vector[2]))
 
-lattice = lattice_parameter(primitive_vector, crys_str=cst)
-#lattice = np.divide(np.array([a[0],b[1],c[2]]),S_factor)*(factor*2.0) # Use the projection of lattice vectors to get lattice constants
-print('(Conventional) Unit cell lattice constants: \n')
-print('Lx\t=\t%7.5f\nLy\t=\t%7.5f\nLz\t=\t%7.5f\n'%(lattice[1],lattice[0],lattice[2]))
+		print("Note: The convention used here for lattice parameters calculation:\n\t 'a' should be in xy-plane \n\t 'b' should be in yz-plane \n\t 'c' hsould be in xz-plane.\n")
+		print("** WARNING: If you don't follow above convention then depending on how you define lattice vectors the dummy index x, y, z in L may change\n")
+		print("** You might need to rotate the coordinate system to follow the convention. Please check the lattice vectors in input file (e.g. POSCAR).\n")
+
+		rotate = input("If you need the rotation please provide here the number of rotation you need to get to the above condition. Else press 'ENTER'.\n") 
+
+		if(rotate):
+			primitive_vector = np.roll(primitive_vector,int(rotate))
+
+		lattice = lattice_parameter(primitive_vector, crys_str=cst)
+		#lattice = np.divide(np.array([a[0],b[1],c[2]]),S_factor)*(factor*2.0) # Use the projection of lattice vectors to get lattice constants
+	print('(Conventional) Unit cell lattice constants: \n')
+	print('Lx\t=\t%7.5f\nLy\t=\t%7.5f\nLz\t=\t%7.5f\n'%(lattice[0],lattice[1],lattice[2]))
